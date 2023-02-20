e0c124a9d8e96c4d0f89ac3d41ad647cb715a4da
GuruMahan <iosdeve2022@gmail.com>
2023-02-20 15:53:46 +0530

    Initial Commit

---
+++ ImageSizeCompressPOC/ImageSizeCompressPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSizeCompressPOCApp.swift
+//  ImageSizeCompressPOC
+//
+//  Created by Guru Mahan on 20/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageSizeCompressPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
